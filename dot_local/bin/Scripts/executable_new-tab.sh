f719d79f98e9c9b9a628e94a52c1fc655c160486
Richard Hajek <richard.m.hajek@gmail.com>
2026-01-11 15:41:31 +0100

    Update .bashrc Update .config/git/ignore Update .config/kitty/kitty.conf Update .config/shellrc_aliases Update .config/shellrc_apps Update .local/bin/Scripts/new-tab.sh

--- dot_local/bin/Scripts/executable_new-tab.sh
+++ dot_local/bin/Scripts/executable_new-tab.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-exec >> /tmp/kitty_script.log 2>&1
+# exec >> /tmp/kitty_script.log 2>&1
 
-if pgrep -x "kitty" > /dev/null; then
-    kitten @ --to unix:$(echo -n /tmp/mykitty.sock* )  launch --type tab & disown
+if pgrep "kitty" > /dev/null; then
+    kitten @ --to unix:$(echo -n ${HOME}/.cache/mykitty.sock* )  launch --type tab & disown
 else
-    /usr/bin/kitty & disown
+    kitty & disown
 fi
 
