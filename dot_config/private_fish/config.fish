3def9bb8f4e0a5bb3a09b865118881f6df71b6a7
Richard Hajek <richard.m.hajek@gmail.com>
2026-05-06 03:35:53 +0200

    Add .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    
+    # https://stackoverflow.com/questions/13995857/suppress-or-customize-intro-message-in-fish-shell
+    set fish_greeting
+
+    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+end
